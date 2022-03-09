055b4b56418370b51f4f4a9b8d4ff503cc974fe3
minhtri06ltr <laptopdienthoai1@gmail.com>
2022-03-09 12:12:30 +0000

    navbar animation + mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,27 @@
 import logo from "../../images/logo.png";
-import { HiMenuAlt4, HiX, HiArrowSmRight } from "react-icons/hi";
+import {
+  HiMenuAlt4,
+  HiX,
+  HiOutlineChevronRight,
+  HiAcademicCap,
+} from "react-icons/hi";
+import { GrBitcoin } from "react-icons/gr";
+import { GiCardExchange } from "react-icons/gi";
+import { FaWallet } from "react-icons/fa";
 import { useState } from "react";
 
-const NavbarItem = ({ title, classNameProps }) => {
-  return <li className={`mx-4 cursor-pointer ${classNameProps}`}>{title}</li>;
+const NavbarItem = ({ title, classNameProps, icon }) => {
+  return (
+    <>
+      <li
+        className={`mx-4 transition ease-linear duration-300 hover:opacity-60 text-lg cursor-pointer ${classNameProps}`}
+      >
+        {icon}
+        {title}
+        <HiOutlineChevronRight className="md:hidden ml-auto" fontSize={28} />
+      </li>
+    </>
+  );
 };
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -14,44 +32,65 @@ const Navbar = () => {
       </div>
       <ul className="font-semibold justify-between items-center flex-initial list-none text-white hidden md:flex">
         {["Market", "Exchange", "Tutorial", "Wallet"].map((item, i) => (
-          <NavbarItem
-            title={item}
-            classNameProps="transition ease-linear duration-300 hover:opacity-60"
-            key={i}
-          />
+          <NavbarItem title={item} key={i} />
         ))}
         <li className="neon-button font-bold ml-4">Login</li>
       </ul>
+
       <div className="flex relative">
+        <button className="neon-button md:hidden mr-6 font-bold ml-4">
+          Login
+        </button>
         {toggleMenu ? (
           <HiX
             fontSize={28}
-            className="text-white md:hidden cursor-pointer"
+            className="text-white md:hidden cursor-pointer rotate-center"
             onClick={() => setToggleMenu(false)}
           />
         ) : (
           <HiMenuAlt4
             fontSize={28}
-            className="text-white md:hidden cursor-pointer"
+            className="text-white md:hidden cursor-pointer rotate-center"
             onClick={() => setToggleMenu(true)}
           />
         )}
+
         {toggleMenu && (
-          <ul className="animate-slide-in rounded-md flex flex-col items-center shadow-2xl -top-0 -right-2 text-white font-semibold md:hidden blue-glassmorphism h-screen w-[60vw] fixed">
-            <li
-              className="w-full my-6 ml-6 animate-bounce cursor-pointer"
-              onClick={() => setToggleMenu(false)}
-            >
-              <HiArrowSmRight fontSize={28} />
-            </li>
-            {["Market", "Exchange", "Tutorial", "Wallet"].map((item, i) => (
-              <NavbarItem
-                title={item}
-                key={i}
-                classNameProps="mb-4 transition ease-linear duration-300 hover:opacity-60 text-lg"
-              />
-            ))}
-          </ul>
+          <>
+            <ul className="slide-left px-14 z-10 animate-slide-in rounded-md flex flex-col items-center shadow-2xl -top-0 -right-[6.5rem] text-white font-semibold md:hidden blue-glassmorphism h-screen w-[60vw] fixed">
+              <li
+                className="self-start my-6 -ml-8 animate-bounce cursor-pointer"
+                onClick={() => setToggleMenu(false)}
+              >
+                <HiX fontSize={28} />
+              </li>
+              {[
+                {
+                  title: "Market",
+                  icon: <GrBitcoin fontSize={28} className="mr-14" />,
+                },
+                {
+                  title: "Exchange",
+                  icon: <GiCardExchange fontSize={28} className="mr-14" />,
+                },
+                {
+                  title: "Tutorial",
+                  icon: <HiAcademicCap fontSize={28} className="mr-14" />,
+                },
+                {
+                  title: "Wallet",
+                  icon: <FaWallet fontSize={28} className="mr-14" />,
+                },
+              ].map((item, i) => (
+                <NavbarItem
+                  title={item.title}
+                  icon={item.icon}
+                  key={i}
+                  classNameProps="w-full flex items-center mt-12 "
+                />
+              ))}
+            </ul>
+          </>
         )}
       </div>
     </nav>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    hmr: {
+      host: "3000-minhtri06ltr-web30-ux3bldiba3n.ws-us34.gitpod.io/:3000",
+      port: 3000,
+      protocol: "wss",
+    },
+  },
+});
